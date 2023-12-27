0ce645025c20b512c762a1fd8493ed336c0b9cf4
navtesh21 <makennavtesh@gmail.com>
2023-12-28 01:49:01 +0530

    some feature changed

--- js/scripts.js
+++ js/scripts.js
@@ -10,32 +10,6 @@ const global = {
     api_key: '2f9e8a399997776527a6fb991b23cd51'
 }
 
-//trying disqus
-function disqus() {
-   const showId = window.location.search.split("=")[1]
-   document.querySelector(".comment").innerHTML = `   <div id="disqus_thread"></div>
-   <script>
-       /**
-       *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-       *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-       
-       var disqus_config = function () {
-       this.page.url = '${window.location.href}';  // Replace PAGE_URL with your page's canonical URL variable
-       this.page.identifier = '${showId}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-       };
-       
-       (function() { // DON'T EDIT BELOW THIS LINE
-       var d = document, s = d.createElement('script');
-       s.src = 'https://flixx-app-3.disqus.com/embed.js';
-       s.setAttribute('data-timestamp', +new Date());
-       (d.head || d.body).appendChild(s);
-       })();
-   </script>
-   <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>`
-   
-   console.log("hello")
-
-}
 
 // Function to create a new guest session
 async function createGuestSession() {
@@ -564,11 +538,9 @@ function init() {
         case "/flixx-app/":
             displayPopularMovies();
             displaySlider();
-            
             break;    
         case "/flixx-app/movie-details.html"  :
             getMovieDetails();
-            disqus();
             break; 
         case "/flixx-app/shows.html":
             displayPopularShows()
